ff37756aee94be2eed8425d6cf0dd8b5e6845f86
Jescas981 <jescas981@gmail.com>
2023-03-27 08:23:35 -0500

    Add MSC support
    	modified:   .gitignore
    	modified:   dynamic_array/include/dsc/cdynarray.h
    	modified:   dynamic_array/src/cdynarray.c
    	modified:   dynamic_array/test/dynarray.c

--- dynamic_array/include/dsc/cdynarray.h
+++ dynamic_array/include/dsc/cdynarray.h
@@ -6,12 +6,15 @@
 
 #define DYNARRAY_GROW 3
 
+<<<<<<< HEAD
 #ifdef _MSC_VER
 #define DYNARRAY_TYPEOF(value) _typeof(value)
 #else
 #define DYNARRAY_TYPEOF(value) typeof(value)
 #endif
 
+=======
+>>>>>>> 0f2bc86 (Add MSC support)
 typedef struct dynarray_data dynarray_data;
 // Dont' use this type in your code, it only works to make this library generic
 typedef void* dynarray;
@@ -26,7 +29,23 @@ dynarray dynarray_create(void);
  * @brief Delete Dynamic Array
  * @param arr Dynamic Array to free
  */
+<<<<<<< HEAD
 dynarray dynarray_free(dynarray arr);
+=======
+void dynarray_free(dynarray arr);
+
+/**
+ * @brief Length of Dynamic Array
+ * @param arr Dynarray pointer
+ */
+size_t dynarray_length(dynarray arr);
+
+/**
+ * @brief Capacity of Dynamic Array
+ * @param arr Dynarray pointer
+ */
+size_t dynarray_capacity(dynarray arr);
+>>>>>>> 0f2bc86 (Add MSC support)
 
 /**
  * @brief Allocates a new Dynarray in heap with reserved capacity
@@ -36,16 +55,37 @@ dynarray dynarray_free(dynarray arr);
  */
 dynarray dynarray_alloc(size_t capacity, size_t size_type);
 
+<<<<<<< HEAD
+=======
+#ifndef _MSC_VER
+>>>>>>> 0f2bc86 (Add MSC support)
 /**
  * @brief Add a new elemnent ot dynamic array
  * @param dynarray_ptr Dynarray pointer
  * @param value Item to add
  * @attention This function can make previous pointers invalid
  */
+<<<<<<< HEAD
 
 #define dynarray_add(dynarray_ptr, value)                           \
   *((DYNARRAY_TYPEOF(value)*)_dynarray_add((dynarray*)dynarray_ptr, \
                                            sizeof(value))) = value;
+=======
+#define dynarray_add(dynarray_ptr, value)                                    \
+  *((typeof(value)*)_dynarray_add((dynarray*)dynarray_ptr, sizeof(typeof(value)))) = \
+      value;
+#else
+/**
+ * @brief Add a new elemnent ot dynamic array
+ * @param dynarray_ptr Dynarray pointer
+ * @param value Item to add
+ * @param type Type of item
+ * @attention This function can make previous pointers invalid
+ */
+#define dynarray_add(dynarray_ptr, value, type) \
+  *((type*)_dynarray_add((dynarray*)dynarray_ptr, sizeof((type)value))) = value;
+#endif  // !1
+>>>>>>> 0f2bc86 (Add MSC support)
 
 /**
  * @brief Add an empty memory block to dynamic array
--- dynamic_array/test/dynarray.c
+++ dynamic_array/test/dynarray.c
@@ -8,11 +8,20 @@ void tearDown(void) {}
 
 void test_adt(void) {
   int* arr = dynarray_create();
+<<<<<<< HEAD
   dynarray_add(&arr, 15);
   dynarray_add(&arr, 45);
   printf("%i", arr[0]);
   printf("%i", arr[1]);
   printf("hola");
+=======
+  for (size_t i = 0; i < 100; i++) {
+    dynarray_add(&arr, (i + 1) * 15, int);
+  }
+  for (size_t i = 0; i < dynarray_length(arr); i++) {
+    printf("%i ", arr[i]);
+  }
+>>>>>>> 0f2bc86 (Add MSC support)
 }
 
 int main(int argc, char* argv[]) {
